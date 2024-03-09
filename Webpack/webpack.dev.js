fae47d7d67c674cecfb1e2ec14b57046c1a3877c
Marina Moiseenko <marina.moiseenko@list.ru>
2024-03-09 12:38:56 +0300

    webpack testing (react)

---
+++ Webpack/webpack.dev.js
@@ -0,0 +1,18 @@
+//webpack.dev.js
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const path = require('path'); //для того чтобы превратить относительный путь в абсолютный, мы будем использовать пакет path
+module.exports = {
+    mode: 'development',
+    devtool: 'eval-source-map',
+    devServer: {
+        static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
+        // compress: true, // это ускорит загрузку в режиме разработки
+        port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
+        open: true, // сайт будет открываться сам при запуске npm run dev
+        hot: true,
+    },
+    plugins: [
+        new ReactRefreshWebpackPlugin(),
+    ],
+}
+
